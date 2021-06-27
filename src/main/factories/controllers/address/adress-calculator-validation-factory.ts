c140d181bdda567a800d8ebcbd9cd53c5a4ed75d
Aryel de Sá <aryel.santos@spassu.com>
2021-06-26 23:26:35 -0300

    refactor: update required field name on address controller

--- src/main/factories/controllers/address/adress-calculator-validation-factory.ts
+++ src/main/factories/controllers/address/adress-calculator-validation-factory.ts
@@ -3,7 +3,7 @@ import { Validation } from '@/presentation/protocols/validation'
 
 export const makeAddressCalculatorValidation = (): ValidationComposite => {
   const validations: Validation[] = []
-  for (const field of ['address']) {
+  for (const field of ['addresses']) {
     validations.push(new RequiredFieldValidation(field))
   }
   return new ValidationComposite(validations)
