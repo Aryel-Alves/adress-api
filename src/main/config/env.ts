ccffe384dc743f1080473fa84d8979a6cb6c8488
Aryel de Sá <aryel.santos@spassu.com>
2021-06-26 22:38:22 -0300

    chore: config env vars

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
-  googleApiToken: process.env.GOOGLE_GEOLOCATION_TOKEN || 'PUT_YOUR_TOKEN_HERE'
+  googleGeolocationApiToken: process.env.GOOGLE_GEOLOCATION_TOKEN || 'PUT_YOUR_TOKEN_HERE',
+  googleGeolocationApiURL: process.env.GOOGLE_GEOLOCATION_URL || 'https://maps.googleapis.com/maps/api/geocode/json'
 }
